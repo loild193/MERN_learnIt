05d2bb98bb554dce8c2c942df3f56c53af3428c2
LeDucLoi193 <dekaito193@gmail.com>
2021-05-07 10:45:28 +0700

    add update and delete post api

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -60,3 +60,82 @@ module.exports.getPost = async (req, res) => {
 		});
 	}
 }
+
+// @route PUT api/posts
+// @desc Update post
+// @access Private
+module.exports.updatePost = async (req, res) => {
+	const { title, description, url, status } = req.body;
+
+	// Simple validation
+	if (!title)
+		return res.status(400).json({
+			success: false,
+			message: "Title is required",
+		});
+	
+	try {
+		let updatedPost = {
+			title,
+			description: description || '',
+			url: (url.startsWith('https://') ? url : `https://${url}` || ''),
+			status: status || 'TO_LEARN',
+		};
+		const postUpdateCondition = {
+			_id: req.params.id,
+			user: req.userId,
+		};
+
+		updatedPost = await Post.findOneAndUpdate(postUpdateCondition, updatedPost, { new: true });
+		
+		// User not authorized to update post or post not found
+		if (!updatedPost)
+			return res.status(401).json({
+				success: false,
+				message: "Post not found or user not authorized",
+			})
+
+		res.json({
+			success: true,
+			message: "Exellent progress",
+			post: updatedPost,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Internal erver error",
+		});
+	}
+}
+
+// @route DELETE api/posts
+// @desc Delete post
+// @access Private
+module.exports.deletePost = async (req, res) => {
+	try {
+		const postDeleteCondition = {
+			_id: req.params.id,
+			user: req.userId,
+		};
+		const deletedPost = await Post.findOneAndDelete(postDeleteCondition);
+
+		// User not authorized to update post or post not found
+		if (!deletedPost)
+			return res.status(401).json({
+				success: false,
+				message: "Post not found or user not authorised",
+			});
+
+		res.json({
+			success: true,
+			post: deletedPost,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Internal erver error",
+		});
+	}
+}
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -4,5 +4,7 @@ const postController = require('../controllers/post.controller');
 
 router.post('/', postController.createPost);
 router.get('/', postController.getPost);
+router.put('/:id', postController.updatePost);
+router.delete('/:id', postController.deletePost);
 
 module.exports = router

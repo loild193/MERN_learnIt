d40b93316e61e74e303b94e4dac946d5445f4255
LeDucLoi193 <dekaito193@gmail.com>
2021-05-12 22:58:58 +0700

    fix cors v4

--- server/index.js
+++ server/index.js
@@ -12,9 +12,14 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.options('*', cors());
+// app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://floating-dawn-15563.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/auth', authRouter);
 app.use('/api/posts', authMiddleware.verifyToken, postRouter);
 
